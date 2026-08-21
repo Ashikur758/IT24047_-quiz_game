efaf5bdb485e1bc7f3420bb0a84b7de5dff79568
MD ASHIKUR RHAMAN ASHIK <ashikurrhaman724@gmail.com>
2026-08-21 09:42:17 +0600

    Update DBConnection.java

--- districe_quiz/src/main/java/com/example/DBConnection.java
+++ districe_quiz/src/main/java/com/example/DBConnection.java
@@ -9,9 +9,9 @@ public class DBConnection {
     private static final String URL =
             "jdbc:mysql://localhost:3306/districe_quiz";
 
-    private static final String USER = "root";
+    private static final String USER = "";
 
-    private static final String PASSWORD = "68253@As";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection()
             throws SQLException {
